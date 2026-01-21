216cc4f14265abe04e34e9d2b339ade8683bae6e
Opilane TTHK <opilane@tthk.ee>
2026-01-21 17:18:36 +0200

    Kalkulaator

---
+++ Kalkulaator/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Kalkulaator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kas su tuba on kolmnurga, ristküliku või ringi kujuline?");
+            string toakuju = Console.ReadLine();
+            if (toakuju == "kolmnurk")
+            {
+                Console.WriteLine($"Mis on sinu esimese külje pikkus");
+                double külg_a = double.Parse( Console.ReadLine() );
+            }
+        }
+    }
+}
